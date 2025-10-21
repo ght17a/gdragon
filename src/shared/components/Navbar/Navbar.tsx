398ee40279bc8261e43f18e8fe5527f706d96af9
ght17a <hubert.truong01@gmail.com>
2025-10-21 16:56:20 +0200

    Add navbar visual

---
+++ src/shared/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import './Navbar.css';
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Discography</li>
+                <li>News</li>
+                <img src="src\assets\logo-white.png"/>
+                <li>Fashion</li>
+                <li>Peaceminusone Archive</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
